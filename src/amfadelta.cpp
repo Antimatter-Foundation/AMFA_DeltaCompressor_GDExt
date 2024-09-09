4f38f1f6c553d95e8964239a17a44e0f0e39616b
ashley <vectorassembly@icloud.com>
2024-09-09 17:40:44 +1100

    hdiff includes

--- src/amfadelta.cpp
+++ src/amfadelta.cpp
@@ -6,10 +6,13 @@
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/string.hpp>
 
+#include "HDiffPatch/libHDiffPatch/HDiff/diff.h"
+#include "HDiffPatch/libHDiffPatch/HPatch/patch.h"
+
 using namespace godot;
 
 void DeltaCompressor::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("get_version"), &DeltaCompressor::get_version);
+    //ClassDB::bind_method(D_METHOD("get_version"), &DeltaCompressor::get_version);
 }
 
 DeltaCompressor::DeltaCompressor() {
