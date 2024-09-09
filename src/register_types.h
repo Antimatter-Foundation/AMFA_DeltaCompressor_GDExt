8ed4ad9a7cdfb7a6926120b5a45bd42f523c7b68
ashley <vectorassembly@icloud.com>
2024-09-09 16:48:30 +1100

    Clear example stuff

--- src/register_types.h
+++ src/register_types.h
@@ -2,14 +2,14 @@
 // Created by vectorassembly on 9/9/24.
 //
 
-#ifndef GDEXAMPLE_REGISTER_TYPES_H
-#define GDEXAMPLE_REGISTER_TYPES_H
+#ifndef AMFADELTA_REGISTER_TYPES_H
+#define AMFADELTA_REGISTER_TYPES_H
 
 #include <godot_cpp/core/class_db.hpp>
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level);
-void uninitialize_example_module(ModuleInitializationLevel p_level);
+void initialize(ModuleInitializationLevel p_level);
+void uninitialize(ModuleInitializationLevel p_level);
 
-#endif // GDEXAMPLE_REGISTER_TYPES_H
+#endif
